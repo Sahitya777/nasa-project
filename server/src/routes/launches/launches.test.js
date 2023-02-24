91fd6c72eacaceb10a5630a82b0fac541a2738ef
Sahitya77 <sahityanijhawan@gmail.com>
2023-02-24 12:18:22 +0530

    changes

--- server/src/routes/launches/launches.test.js
+++ server/src/routes/launches/launches.test.js
@@ -1,45 +1,94 @@
-const requet=require('supertest')
-const app=require('../../app');
-const { loadLaunchesData } = require('../../models/launches.model');
-const {mongoConnect,mongoDisconnect} =require('../../services/mongo');
-jest.setTimeout(50000);
+const request = require('supertest');
+const app = require('../../app');
+const { 
+  mongoConnect,
+  mongoDisconnect,
+} = require('../../services/mongo');
+const {
+  loadPlanetsData,
+} = require('../../models/planets.model');
 
-describe('Launches API',()=>{
-    beforeAll(async()=>{
-        await mongoConnect();
-        await loadLaunchesData();
-        jest.setTimeout(50000);
-    },50000);
-    afterAll(async()=>{
-        await mongoDisconnect();
-        jest.setTimeout(50000);
-    })
-    describe('test GET/launches',()=>{
-        test('It should respond with 200 success',async ()=>{
-            const response=await requet(app).get('/launches');
-            expect(response.statusCode).toBe(200);
-        });
-    },50000)
-    
-    describe('Test POST/launch',()=>{
-        test('It should response with 200 success',async()=>{
-            const response=await requet(app).post('/launches').send({
-                mission:'USS Enterprise',
-                rocket:'NCC 1701-D',
-                destination:'Kepler-62 f',
-                launchDate:'January 5, 2030',
-            })
-            .expect('Content-Type',/json/)
-            .expect(201)
-    
-            // expect(response.body).toMatchObject({
-            //     mission:'USS Enterprise',
-            //     rocket:'NCC 1701-D',
-            //     destination:'Kepler-62 f',
-            //     launchDate:'January 5, 2030',
-            // });
-        });
-        test('It should catch missing required properties',()=>{})
-        test('It should catch invalid dates',()=>{})
-    },50000)
-})
+
+
+describe('Launches API', () => {
+  beforeAll(async () => {
+    await mongoConnect();
+    await loadPlanetsData();
+  },30000);
+
+  afterAll(async () => {
+    await mongoDisconnect();
+    jest.setTimeout(() => {
+        
+    }, 30000);
+  },30000);
+
+  describe('Test GET /launches', () => {
+    test('It should respond with 200 success', async () => {
+      const response = await request(app)
+        .get('/launches')
+        .expect('Content-Type', /json/)
+        .expect(200);
+    });
+  },30000);
+  
+  describe('Test POST /launch', () => {
+    const completeLaunchData = {
+      mission: 'USS Enterprise',
+      rocket: 'NCC 1701-D',
+      target: 'Kepler-62 f',
+      launchDate: 'January 4, 2028',
+    };
+  
+    const launchDataWithoutDate = {
+      mission: 'USS Enterprise',
+      rocket: 'NCC 1701-D',
+      target: 'Kepler-62 f',
+    };
+  
+    const launchDataWithInvalidDate = {
+      mission: 'USS Enterprise',
+      rocket: 'NCC 1701-D',
+      target: 'Kepler-62 f',
+      launchDate: 'zoot',
+    };
+  
+    test('It should respond with 201 created', async () => {
+      const response = await request(app)
+        .post('/launches')
+        .send(completeLaunchData)
+        .expect('Content-Type', /json/)
+        .expect(201);
+  
+      const requestDate = new Date(completeLaunchData.launchDate).valueOf();
+      const responseDate = new Date(response.body.launchDate).valueOf();
+      expect(responseDate).toBe(requestDate);
+  
+      expect(response.body).toMatchObject(launchDataWithoutDate);
+    });
+  
+    test('It should catch missing required properties', async () => {
+      const response = await request(app)
+        .post('/launches')
+        .send(launchDataWithoutDate)
+        .expect('Content-Type', /json/)
+        .expect(400);
+  
+      expect(response.body).toStrictEqual({
+        error: 'Missing required launch property',
+      });
+    });
+  
+    test('It should catch invalid dates', async () => {
+      const response = await request(app)
+        .post('/launches')
+        .send(launchDataWithInvalidDate)
+        .expect('Content-Type', /json/)
+        .expect(400);
+  
+      expect(response.body).toStrictEqual({
+        error: 'Invalid launch date',
+      });
+    });
+  },30000);
+});
